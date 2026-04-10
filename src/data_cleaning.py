7dfed8c0204c8b75b8ed6feee9d9fec694fbc4dc
Shettiga <sudarshan.i.shettigar@gmail.com>
2026-04-10 11:32:27 +0530

    created the clean_data.csv

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -1,33 +1,27 @@
+# Show first rows
 import pandas as pd
 
 # Load dataset
-df = pd.read_csv("data/raw/phishing_dataset.csv")
+df = pd.read_csv("data/raw/PhishingData.csv")
 
-# Show first rows
 print("First 5 rows:")
 print(df.head())
 
-# Basic info
 print("\nDataset Info:")
 print(df.info())
 
-# Remove missing values
+# Clean data
 df = df.dropna()
-
-# Remove duplicates
 df = df.drop_duplicates()
 
-# Rename columns (if needed)
-df.columns = ['url', 'label']
+# Rename target column
+df = df.rename(columns={"Result": "label"})
 
 # Check distribution
 print("\nLabel Distribution:")
 print(df['label'].value_counts())
 
-# Add feature (optional)
-df['url_length'] = df['url'].apply(len)
-
 # Save cleaned dataset
 df.to_csv("data/processed/cleaned_data.csv", index=False)
 
-print("\n✅ Data cleaned and saved successfully!")
+print("\n✅ Data cleaned successfully!")
